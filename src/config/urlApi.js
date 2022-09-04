e0a57038cc712eb28d3d636467862ed3e61608bb
ahtaum <aditalmahdi@gmail.com>
2022-09-04 08:18:23 +0700

    MOVE PLACE FROM HEROKU TO RAILWAYS URL API

--- src/config/urlApi.js
+++ src/config/urlApi.js
@@ -1,5 +1,6 @@
 let urlApi = {
-    "apiUrl": `https://my-daily-backend.herokuapp.com/api/`
+    // "apiUrl": `https://my-daily-backend.herokuapp.com/api/`, // this is use HEROKU
+    "apiUrl": `https://my-daily-backend-production.up.railway.app/api/`, // this is use RAILWAYS
 }
 
 export default urlApi
